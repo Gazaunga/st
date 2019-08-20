2ec5f24b497570d99d4a69677c51b7afd2449967
Jeremy Ottley <Gazaunga@gmail.com>
2019-08-20 17:45:13 -0400

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "SanFranciscoText-Regular:pixelsize=14:antialias=true:autohint=true";
+static char font[] = "Hasklig-Regular:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
